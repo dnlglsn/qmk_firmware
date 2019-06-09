aa6e5ee407cd74dd663be746e142752967adacb7
Daniel Gleason <danielg@surveymonkey.com>
2019-06-08 22:33:36 -0700

    Added function right space as delete and function left space as backspace

--- keyboards/dnlglsn/voidstar/keymaps/bB_rA_pC_gF_oA_yB/keymap.c
+++ keyboards/dnlglsn/voidstar/keymaps/bB_rA_pC_gF_oA_yB/keymap.c
@@ -47,7 +47,7 @@ XXXXXXX, XXXXXXX, XXXXXXX, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   
 XXXXXXX, XXXXXXX, XXXXXXX, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   		KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, XXXXXXX, KC_BSLS, XXXXXXX,
 XXXXXXX, XXXXXXX, XXXXXXX, KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX,		KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX, KC_ENT,  XXXXXXX, XXXXXXX,
 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   		KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, ZZ_UP,   KC_DEL,  XXXXXXX,
-XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, KC_LGUI, MO(_FN), XXXXXXX, A_SPC,   XXXXXXX, XXXXXXX,		XXXXXXX, KC_SPC,  XXXXXXX, KC_RALT, ZZ_CTRL, XXXXXXX, KC_LEFT, ZZ_DOWN, KC_RGHT, XXXXXXX 
+XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, KC_LGUI, MO(_FN), XXXXXXX, A_SPC,   XXXXXXX, XXXXXXX,		XXXXXXX, KC_SPC,  XXXXXXX, MO(_FN), ZZ_CTRL, XXXXXXX, KC_LEFT, ZZ_DOWN, KC_RGHT, XXXXXXX 
 ),
 
 // For games, disable GUI key and make the left spacebar normal so I can hold it down.
@@ -66,7 +66,7 @@ _______, _______, _______, KC_MUTE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  
 _______, _______, _______, KC_VOLU, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______,		_______, _______, KC_PGUP, _______, KC_PSCR, _______, _______, XXXXXXX, KC_F5,   _______,
 _______, _______, _______, KC_VOLD, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, XXXXXXX,		_______, KC_HOME, KC_PGDN, KC_END,  _______, _______, XXXXXXX, L_GAME,  XXXXXXX, _______,
 _______, _______, _______, XXXXXXX, _______, _______, _______, KC_CAPS, _______, _______,		_______, _______, _______, _______, _______, _______, _______, KC_PGUP, _______, _______,
-_______, _______, _______, _______, KC_APP,  _______, _______, _______, XXXXXXX, XXXXXXX,		XXXXXXX, KC_MPLY, XXXXXXX, _______, _______, _______, KC_HOME, KC_PGDN, KC_END,  _______ 
+_______, _______, _______, _______, KC_APP,  _______, _______, KC_DEL,  XXXXXXX, XXXXXXX,		XXXXXXX, KC_BSPC, XXXXXXX, _______, _______, _______, KC_HOME, KC_PGDN, KC_END,  _______ 
 ),
 
 [_FG] = LAYOUT( \
