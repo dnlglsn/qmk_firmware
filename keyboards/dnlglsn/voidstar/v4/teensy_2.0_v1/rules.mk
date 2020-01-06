31f73af3270afd82418f46af813c02a502e084f9
Daniel Gleason <danielg@surveymonkey.com>
2020-01-05 22:03:44 -0800

    blah:

--- keyboards/dnlglsn/voidstar/v4/teensy_2.0_v1/rules.mk
+++ keyboards/dnlglsn/voidstar/v4/teensy_2.0_v1/rules.mk
@@ -4,7 +4,6 @@ ARCH = AVR8
 F_USB = $(F_CPU)
 BOOTLOADER = halfkay # halfkay for Teensy 2.0
 OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
-OPT_DEFS += -DBOOTLOADER_SIZE=512
 SPLIT_KEYBOARD = yes
 MOUSEKEY_ENABLE = yes # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes # Audio control and System control(+450)
